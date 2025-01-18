83df337128865bbe764a438a5459e7e50ca55b61
Yevhen999 <gamingproduct89@gmail.com>
2025-01-18 08:58:38 -0500

    test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+// import { NavLink } from "react-router-dom";
+
+import icon from "../../img/main-icon.png";
+import theme from "../../img/theme.png";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <nav>
+        <img src={icon} alt="main-icon" />
+        <p>MENU</p>
+      </nav>
+      <button>
+        <img src={theme} alt="theme" />
+      </button>
+    </header>
+  );
+};
